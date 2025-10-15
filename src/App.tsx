a883ef8a74f09f01ae96261de8ad8ca274cd4202
= <=>
2025-10-15 01:54:18 -0600

    dist y src

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { MainProvider } from "./context/mainProvider";
+import Navbar from "./components/common/nav-bar/nav-bar";
+import Dashboard from "./components/dashboard/Dashboard";
+import DebtTable from "./components/debts/debt-table";
+import RemindersList from "./components/reminders/reminders-list";
+import ReportsView from "./components/reports/reports-view";
+
+const App: React.FC = () => {
+  return (
+    <main>
+      hello mi appp appp.tsx
+    </main>
+  );
+};
+
+export default App;
